9a98e7cd47f073e02a417d5c98dd59afbd8932fe
pwityk <pawelwityk@student.agh.edu.pl>
2023-01-15 16:51:19 +0100

    changes in variables

--- src/ParsingData.java
+++ src/ParsingData.java
@@ -3,7 +3,6 @@ import org.apache.logging.log4j.Logger;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.FileOutputStream;

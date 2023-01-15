f2200b4f5d49c6f78b4d52740110c57e25c533dc
pwityk <pawelwityk@student.agh.edu.pl>
2023-01-15 14:37:16 +0100

    changes in structure, working table, clear code

--- src/GraphicInterfaceTable.java
+++ src/GraphicInterfaceTable.java
@@ -13,27 +13,12 @@ public class GraphicInterfaceTable extends JFrame {
     private final static Logger log = LogManager.getLogger(GraphicInterfaceTable.class);
     public static void createAndShowGUI(ArrayList<List<String>> data) {
         log.info("Created AcionListener");
-        ActionListener myActionListener = new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String buttonPressed = e.getActionCommand();
-            }
+        ActionListener myActionListener = e -> {
+            String buttonPressed = e.getActionCommand();
         };
+
         Object[] column = {"Data","Cena hurtowa","Opłata paliwowa","Akcyza"};
         DefaultTableModel tableModel = new DefaultTableModel(column, 0);
-
-        JFrame jf = new JFrame("Hurtowe ceny paliw");
-        JPanel jp = new JPanel(new GridLayout(0, 1));
-        JTable jt = new JTable(tableModel);
-        Dimension dm = new Dimension(200, 300);
-
-        jp.setOpaque(true);
-
-        jt.setDragEnabled(false);
-        jt.setAutoCreateRowSorter(true);
-        jt.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-        jp.setFocusable(false);
-        jp.add(jt);
-        jp.add(new JScrollPane(jt));
 
         List<Object[]> list = new ArrayList<>();
 
@@ -43,12 +28,24 @@ public class GraphicInterfaceTable extends JFrame {
             tableModel.addRow(tempObj);
         }
 
+        JFrame jf = new JFrame("Hurtowe ceny paliw");
+        JPanel jp = new JPanel(new GridLayout(0, 1));
+        JTable jt = new JTable(tableModel);
+
+        jt.setDragEnabled(false);
+        jt.setAutoCreateRowSorter(true);
+        jt.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+
+        jp.setOpaque(true);
+        jp.setFocusable(false);
+        jp.add(jt);
+        jp.add(new JScrollPane(jt));
+
         jf.setResizable(true);
         jf.setLocationRelativeTo(null);
         jf.getContentPane().add(jp);
         jf.setVisible(true);
         jf.pack();
-
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
